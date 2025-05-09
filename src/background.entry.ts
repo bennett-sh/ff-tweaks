82e055004e0115698ee5a7ea5cc9c5ad02a2de04
Bennett <no-reply@bennettsh.de>
2025-05-09 18:33:30 +0200

    add gemini switch

--- src/background.entry.ts
+++ src/background.entry.ts
@@ -3,21 +3,45 @@ browser.tabs.onCreated.addListener(tab => {
     if (tabId === tab.id && changeInfo.url) {
       const url = new URL(changeInfo.url)
 
-      if (url.hostname === 'chatgpt.com' && (url.pathname === '/' || url.pathname.startsWith('/c/'))) {
-        browser.tabs.query({
-          currentWindow: true,
-          pinned: true,
-          url: 'https://chatgpt.com/*'
-        }).then(tabs => {
-          if (!tabs || tabs.length < 1) return
-          const pinnedTab = tabs[0]
-
-          browser.tabs.update(pinnedTab.id, {
-            active: true,
-            url: changeInfo.url // force create new chat
+      if (
+        url.hostname === 'chatgpt.com' &&
+        (url.pathname === '/' || url.pathname.startsWith('/c/'))
+      ) {
+        browser.tabs
+          .query({
+            currentWindow: true,
+            pinned: true,
+            url: 'https://chatgpt.com/*'
           })
-          browser.tabs.remove(tabId)
-        })
+          .then(tabs => {
+            if (!tabs || tabs.length < 1) return
+            const pinnedTab = tabs[0]
+
+            browser.tabs.update(pinnedTab.id, {
+              active: true,
+              url: changeInfo.url // force create new chat
+            })
+            browser.tabs.remove(tabId)
+          })
+      }
+
+      if (url.hostname === 'gemini.google.com') {
+        browser.tabs
+          .query({
+            currentWindow: true,
+            pinned: true,
+            url: 'https://gemini.google.com/*'
+          })
+          .then(tabs => {
+            if (!tabs || tabs.length < 1) return
+            const pinnedTab = tabs[0]
+
+            browser.tabs.update(pinnedTab.id, {
+              active: true,
+              url: changeInfo.url // force create new chat
+            })
+            browser.tabs.remove(tabId)
+          })
       }
 
       browser.tabs.onUpdated.removeListener(handleTabUpdate)
